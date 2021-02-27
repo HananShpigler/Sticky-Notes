b1b20e8acf11b2a4d4346ddcc86ad30a3ad8902a
Hanan Shpigler <78700951+HananShpigler@users.noreply.github.com>
2021-02-27 17:42:50 +0200

    Add files via upload

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import EventNoteIcon from "@material-ui/icons/EventNote";
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <EventNoteIcon /> Sticky Notes{" "}
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
